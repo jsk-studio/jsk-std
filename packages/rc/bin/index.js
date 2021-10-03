742b473f7e936ed93f837f70632d42c477091fc4
lynnkoo <lynnkoo.1120@gmail.com>
2021-10-03 16:07:56 +0800

    fix bugs

--- packages/rc/bin/index.js
+++ packages/rc/bin/index.js
@@ -4,20 +4,21 @@ const { copyFilesSync } = require('@jsk-std/io')
 const path = require('path')
 let [ commend, dir ] = args
 dir = dir || rootDir
+
 if (commend === 'sync') {
-    copyFilesSync(path.resolve('./base'), path.resolve(dir), [
+    copyFilesSync(path.resolve(__dirname, '../base'), path.resolve(dir), [
         'tsconfig.prod.json',
         'LICENSE',
         '.npmignore',
     ])
 } 
 if (commend === 'sync-repo') {
-    copyFilesSync(path.resolve('./base'), path.resolve(dir), [
+    copyFilesSync(path.resolve(__dirname, '../base'), path.resolve(dir), [
         '.gitattributes',
         '.gitignore',
         'LICENSE',
     ])
-    copyFilesSync(path.resolve('./repo'), path.resolve(dir), [
+    copyFilesSync(path.resolve(__dirname, '../repo'), path.resolve(dir), [
         'jest.config.js',
         'lerna.json',
         'tsconfig.json',
